09ff08496f5e1fef328f62996cc2df1be23374a1
Guillermo Urcera Martín <g.urcera.martin@gmail.com>
2018-04-16 18:40:13 +0200

    Finished file_to_gazebo

--- src/file_to_gazebo.py
+++ src/file_to_gazebo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 import rospy
-import gazebo_parameters
+import robot_ddpg_agent.src.gazebo_parameters as gazebo_parameters
 from robot_ddpg_gazebo.srv import *
 import csv
 
@@ -11,15 +11,15 @@ t=[]
 f = open("/tmp/kautham_path_to_gazebo.csv","r")
 cr = csv.reader(f,delimiter=';')
 for row in cr:
-	x.append(row[0])
-	y.append(row[1])
-	t.append(row[2])
+        x.append(float(row[0])*10)
+        y.append(float(row[1])*10)
+        t.append(float(row[2]))
 
 rospy.wait_for_service('kautham_loop_service')
 try:			
 	client=rospy.ServiceProxy('kautham_loop_service',KauthamLoopSrv)
 	#float32[] x;float32[] y;float32[] t;int16 num_points;string[] obstacles;int16 num_obstacles;float32[] obstacle_positions;
 	response=client(x,y,t,len(x),gazebo_parameters.OBSTACLE_NAMES,gazebo_parameters.NUM_OBSTACLES,gazebo_parameters.OBSTACLE_POSITIONS)
-	return response.reward
 except rospy.ServiceException, e:
 	print "Service call failed: %s"%e
+print response.reward
