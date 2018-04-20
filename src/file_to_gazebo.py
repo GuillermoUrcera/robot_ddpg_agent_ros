dafe5c14cecef324350fa65b525314e9e29aa9b5
Guillermo <g.urcera.martin@gmail.com>
2018-04-20 02:07:22 +0200

    Updated file_to_gazebo
    Now writes reward, distance and obstacle_displacement to file

--- src/file_to_gazebo.py
+++ src/file_to_gazebo.py
@@ -8,18 +8,25 @@ import csv
 x=[]
 y=[]
 t=[]
-f = open("/tmp/kautham_path_to_gazebo.csv","r")
-cr = csv.reader(f,delimiter=';')
+
+f=open("/tmp/kautham_path_to_gazebo.csv","r")
+cr=csv.reader(f,delimiter=';')
 for row in cr:
         x.append(float(row[0])*10)
         y.append(float(row[1])*10)
         t.append(float(row[2]))
+f.close()
+
+target_file=open("/tmp/kautham_path_in_gazebo.csv","w")
+writer=csv.writer(target_file,delimiter=';') #print: Distance;Obstacle displacement;reward
 
 rospy.wait_for_service('kautham_loop_service')
 try:			
 	client=rospy.ServiceProxy('kautham_loop_service',KauthamLoopSrv)
 	#float32[] x;float32[] y;float32[] t;int16 num_points;string[] obstacles;int16 num_obstacles;float32[] obstacle_positions;
 	response=client(x,y,t,len(x),gazebo_parameters.OBSTACLE_NAMES,gazebo_parameters.NUM_OBSTACLES,gazebo_parameters.OBSTACLE_POSITIONS)
+	writer.writerow([response.distance_covered,response.obstacle_displacement,response.reward])
 except rospy.ServiceException, e:
 	print "Service call failed: %s"%e
-print response.reward
+target_file.close()
+
